845d0e577e74dccb618651342f779581a9b7df7a
Matheus Macedo <matheus.macedo.developer@gmail.com>
2025-02-22 04:41:17 -0300

    feat: merge the pdf dataframes into one

--- class_set_up_assistent/setup.py
+++ class_set_up_assistent/setup.py
@@ -1,16 +1,19 @@
 import tabula
+import pandas
 
 classes_pdf_uri = 'turmas_ofertadas.pdf'
 
-classes_dataframe = tabula.read_pdf(classes_pdf_uri, pages='1', encoding='latin-1')
-classes_dataframe[0].columns = ['Curso', 'Código de turma', 'Turma', 'Teoria', 'Prática', 'Campus', 'Turno', 'T-P-I', 'Vagas totais', 'Vagas veteranos', 'Docente teoria', 'Docente teoria 2', 'Docente prática', 'Docente prática 2']
+classes_dataframe_pdf = tabula.read_pdf(classes_pdf_uri, pages='1', encoding='latin-1')
+classes_dataframe = pandas.concat(classes_dataframe_pdf)
 
-classes_dataframe[0] = classes_dataframe[0].drop('Docente teoria 2', axis=1)
-classes_dataframe[0] = classes_dataframe[0].drop('Docente prática 2', axis=1)
+classes_dataframe.columns = ['Curso', 'Código de turma', 'Turma', 'Teoria', 'Prática', 'Campus', 'Turno', 'T-P-I', 'Vagas totais', 'Vagas veteranos', 'Docente teoria', 'Docente teoria 2', 'Docente prática', 'Docente prática 2']
 
-classes_dataframe[0]['Curso'] = classes_dataframe[0]['Curso'].str.replace('\r', ' ')
-classes_dataframe[0]['Docente teoria'] = classes_dataframe[0]['Docente teoria'].str.replace('\r', ' ')
-classes_dataframe[0]['Docente prática'] = classes_dataframe[0]['Docente prática'].str.replace('\r', ' ')
+classes_dataframe = classes_dataframe.drop('Docente teoria 2', axis=1)
+classes_dataframe = classes_dataframe.drop('Docente prática 2', axis=1)
 
-print(classes_dataframe[0].iloc[0])
-print(classes_dataframe[0].dtypes)
+classes_dataframe['Curso'] = classes_dataframe['Curso'].str.replace('\r', ' ')
+classes_dataframe['Docente teoria'] = classes_dataframe['Docente teoria'].str.replace('\r', ' ')
+classes_dataframe['Docente prática'] = classes_dataframe['Docente prática'].str.replace('\r', ' ')
+
+print(classes_dataframe.iloc[0])
+print(classes_dataframe.dtypes)
